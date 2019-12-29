e3ba66103e6546323a2f40b32f3984837c891e07
Ben Anhalt <ben.anhalt@gmail.com>
2019-12-29 15:19:59 -0600

    finished day 1 part 2.

--- day1.hs
+++ day1.hs
@@ -1,8 +1,16 @@
 
-
+import Data.List
+import qualified Data.Set as S
 
 main :: IO ()
 main = do
   input <- readFile "input1.txt"
-  let vals = (read . dropWhile (== '+')) <$> lines input :: [Int] 
+  let vals = (read . dropWhile (== '+')) <$> lines input :: [Int]
   print $ sum $ vals
+  print $ solve (cycle vals) 0 S.empty
+
+solve :: [Int] -> Int -> S.Set Int -> Int
+solve (v:vs) prev seen =
+  if (v+prev) `elem` seen then (v+prev)
+  else solve vs (v+prev) (S.insert (v+prev) seen)
+
