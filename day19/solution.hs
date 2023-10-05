6bc2abcb614193053e42a163c7170f04262bad95
Ben Anhalt <ben.anhalt@gmal.com>
2023-10-04 23:14:30 -0500

    Imports.

--- day19/solution.hs
+++ day19/solution.hs
@@ -7,11 +7,12 @@ import Data.List.Split (chunksOf)
 import Data.Map.Strict qualified as M
 import Data.Maybe
 import Data.Set qualified as S
+import Data.Vector (Vector, (!), (//))
 import Data.Vector qualified as V
 import Debug.Trace (traceShow, traceShowId)
 import Text.Regex.PCRE ((=~))
 
-type Registers = V.Vector Int
+type Registers = Vector Int
 
 
 type Operand = Int
@@ -20,22 +21,22 @@ type Operation = (String, [Operand] -> Registers -> Registers)
 
 operations :: [Operation]
 operations =
-  [ ("addr", \[a, b, c] regs -> regs V.// [(c, regs V.! a + regs V.! b)]),
-    ("addi", \[a, b, c] regs -> regs V.// [(c, regs V.! a + b)]),
-    ("mulr", \[a, b, c] regs -> regs V.// [(c, regs V.! a * regs V.! b)]),
-    ("muli", \[a, b, c] regs -> regs V.// [(c, regs V.! a * b)]),
-    ("banr", \[a, b, c] regs -> regs V.// [(c, regs V.! a .&. regs V.! b)]),
-    ("bani", \[a, b, c] regs -> regs V.// [(c, regs V.! a .&. b)]),
-    ("borr", \[a, b, c] regs -> regs V.// [(c, regs V.! a .|. regs V.! b)]),
-    ("bori", \[a, b, c] regs -> regs V.// [(c, regs V.! a .|. b)]),
-    ("setr", \[a, _, c] regs -> regs V.// [(c, regs V.! a)]),
-    ("seti", \[a, _, c] regs -> regs V.// [(c, a)]),
-    ("gtir", \[a, b, c] regs -> regs V.// [(c, if a > regs V.! b then 1 else 0)]),
-    ("gtri", \[a, b, c] regs -> regs V.// [(c, if regs V.! a > b then 1 else 0)]),
-    ("gtrr", \[a, b, c] regs -> regs V.// [(c, if regs V.! a > regs V.! b then 1 else 0)]),
-    ("eqir", \[a, b, c] regs -> regs V.// [(c, if a == regs V.! b then 1 else 0)]),
-    ("eqri", \[a, b, c] regs -> regs V.// [(c, if regs V.! a == b then 1 else 0)]),
-    ("eqrr", \[a, b, c] regs -> regs V.// [(c, if regs V.! a == regs V.! b then 1 else 0)])
+  [ ("addr", \[a, b, c] regs -> regs // [(c, regs ! a + regs ! b)]),
+    ("addi", \[a, b, c] regs -> regs // [(c, regs ! a + b)]),
+    ("mulr", \[a, b, c] regs -> regs // [(c, regs ! a * regs ! b)]),
+    ("muli", \[a, b, c] regs -> regs // [(c, regs ! a * b)]),
+    ("banr", \[a, b, c] regs -> regs // [(c, regs ! a .&. regs ! b)]),
+    ("bani", \[a, b, c] regs -> regs // [(c, regs ! a .&. b)]),
+    ("borr", \[a, b, c] regs -> regs // [(c, regs ! a .|. regs ! b)]),
+    ("bori", \[a, b, c] regs -> regs // [(c, regs ! a .|. b)]),
+    ("setr", \[a, _, c] regs -> regs // [(c, regs ! a)]),
+    ("seti", \[a, _, c] regs -> regs // [(c, a)]),
+    ("gtir", \[a, b, c] regs -> regs // [(c, if a > regs ! b then 1 else 0)]),
+    ("gtri", \[a, b, c] regs -> regs // [(c, if regs ! a > b then 1 else 0)]),
+    ("gtrr", \[a, b, c] regs -> regs // [(c, if regs ! a > regs ! b then 1 else 0)]),
+    ("eqir", \[a, b, c] regs -> regs // [(c, if a == regs ! b then 1 else 0)]),
+    ("eqri", \[a, b, c] regs -> regs // [(c, if regs ! a == b then 1 else 0)]),
+    ("eqrr", \[a, b, c] regs -> regs // [(c, if regs ! a == regs ! b then 1 else 0)])
   ]
 
 
@@ -57,9 +58,9 @@ execute :: CPU -> Instruction -> CPU
 --execute cpu@CPU {ip, ipReg} (BindIP r) = cpu { ipReg = r, ip = ip +  }
 execute cpu@CPU {ip, ipReg, regs} inst@(Inst op operands) =
   let Just operation = lookup op operations
-      regs' = operation operands $ regs V.// [(ipReg, ip)]
+      regs' = operation operands $ regs // [(ipReg, ip)]
   in -- traceShow (cpu, inst, regs') $
-  cpu { ip = regs' V.! ipReg + 1, regs = regs' }
+  cpu { ip = regs' ! ipReg + 1, regs = regs' }
 
 run :: CPU -> [Instruction] -> CPU
 run cpu program | ip cpu < 0 || ip cpu >= length program = cpu
